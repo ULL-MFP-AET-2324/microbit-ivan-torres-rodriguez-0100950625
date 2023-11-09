f744f702bd4df797a4c58d71d8cc29e37f4d1122
Ivan <39419547+ivanjtr@users.noreply.github.com>
2023-11-09 18:16:00 +0000

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
-    music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
+    basic.showNumber(randint(0, 10))
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.AB, function () {
+	
 })
